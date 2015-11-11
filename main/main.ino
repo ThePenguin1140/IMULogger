8d2f5a50bc247c20964f0b7a861c8bc524bcbdd5
Jason Wuertz <jwuertz@upei.ca>
2015-11-11 18:23:04 -0400

    doc(main): add TODO

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,14 @@
 void setup() {
-  
+  //TODO setup clock
+  //TODO setup IMU
+  //TODO setup IR Remote
+  //TODO setup various pins (LEDs)
 }
+
+//TODO add vars for logging
 
 void loop() {
-  
+  //TODO monitor IR remote for OK and # press
 }
 
+//TODO format timestamp from clock
